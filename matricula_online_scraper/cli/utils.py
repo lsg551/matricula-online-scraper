05e40e0c4a6dcd63981b27bcdb9666dbaea28f2a
Luis Schulte <63458548+lsg551@users.noreply.github.com>
2024-04-18 14:29:16 +0200

    feat: add a first draft mvp

---
+++ matricula_online_scraper/cli/utils.py
@@ -0,0 +1,35 @@
+"""
+Utils for the CLI.
+"""
+
+from __future__ import annotations
+from urllib.parse import urlparse
+
+
+class URL:
+    """Custom typer CLI argument parser for URLs."""
+
+    def __init__(self, value: str):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return f"<URL: value={self.value}>"
+
+    @staticmethod
+    def is_valid(value: str) -> bool:
+        """Check if a string is a valid URL."""
+        try:
+            result = urlparse(value)
+            return all([result.scheme, result.netloc])
+        except AttributeError:
+            return False
+
+    @staticmethod
+    def from_string(value: str) -> URL:
+        """Create a URL object from a string."""
+        if not URL.is_valid(value):
+            raise ValueError(f"Invalid URL: {value}")
+        return URL(value)
