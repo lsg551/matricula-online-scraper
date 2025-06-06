f973ddb5d30a9d58472f4f3eebf3b8beac46c234
Luis Schulte <git@luisschulte.com>
2025-06-06 21:35:38 +0200

    feat: `shorten_path` utility
    This utility function shortens

---
+++ matricula_online_scraper/utils/shorten_path.py
@@ -0,0 +1,31 @@
+"""Shorten a path to a more user-friendly format."""
+
+from pathlib import Path
+
+
+def shorten_path(path: Path) -> str:
+    """Shortens a given path to a more user-friendly format.
+
+    Examples:
+        >>> shorten_path(Path("/home/user/documents/file.txt"))
+        '~/documents/file.txt'
+
+    Args:
+        path (Path): The path to be shortened.
+
+    Returns:
+        str: Shortend path as string.
+    """
+    home = Path.home()
+    cwd = Path.cwd()
+
+    if path == cwd:
+        short = "."
+    elif cwd in path.parents:
+        short = f"./{path.relative_to(cwd)}"
+    elif home in path.parents:
+        short = f"~/{path.relative_to(home)}"
+    else:
+        short = str(path)
+
+    return short
